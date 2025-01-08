35a92f412eabac89e8d7502b43abd2c2bc4de893
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-01-08 23:32:09 +0530

    Time: 11 ms (10.78%), Space: 44.9 MB (47.84%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,51 +1,58 @@
 class Solution {
-    public boolean isSafe(int row,int col,int n,List<String>board)
-    {
-        for(int i=0;i<col;i++)
+    public List<List<String>> solveNQueens(int n) {
+        List<List<String>>ans=new ArrayList<>();
+        char[][] board=new char[n][n];
+        for(char[] row:board)
         {
-            if(board.get(row).charAt(i)=='Q') return false;
+            Arrays.fill(row,'.');
         }
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
-        {
-            if(board.get(i).charAt(j)=='Q') return false;
-        }
-        for(int i=row,j=col;i<n && j>=0 ;i++,j--)
-        {
-            if(board.get(i).charAt(j)=='Q') return false;
-        }
-        return true;
+        solve(0,n,board,ans);
+        return ans;
     }
-    public void solve(int col,int n,List<String>board,List<List<String>>ans)
+    public void solve(int col,int n,char[][] board,List<List<String>>ans)
     {
         if(col==n)
         {
-            ans.add(new ArrayList<>(board));
+            List<String> one=new ArrayList<>();
+            for(int i=0;i<n;i++)
+            {
+                String row="";
+                for(int j=0;j<n;j++)
+                {
+                    row=row+board[i][j];
+                }
+                one.add(new String(row));
+            }
+            ans.add(new ArrayList<>(one));
             return;
         }
         for(int i=0;i<n;i++)
         {
-            if(isSafe(i,col,n,board))
+            if(isValid(i,col,board))
             {
-                String rowV=board.get(i);
-                String newR=rowV.substring(0,col)+'Q'+rowV.substring(col+1);
-                board.set(i,newR);
+                board[i][col]='Q';
                 solve(col+1,n,board,ans);
-                newR=rowV.substring(0,col)+'.'+rowV.substring(col+1);
-                board.set(i,newR);
+                board[i][col]='.';
             }
         }
-    }
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<>();
-        List<String>board=new ArrayList<>();
-        String row=".".repeat(n);
-        for(int i=0;i<n;i++)
-        {
-            board.add(row);
-        }
-        solve(0,n,board,ans);
-        return ans;
-        
 
+    }
+    public boolean isValid(int row,int col,char[][] board)
+    {
+        int n=board.length;
+        for(int i=0;i<col;i++)
+        {
+            if(board[row][i]=='Q') return false;
+        }
+        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        {
+            if(board[i][j]=='Q') return false;
+        }
+        for(int i=row,j=col;i<n && j>=0;i++,j--)
+        {
+            if(board[i][j]=='Q') return false;
+        }
+
+        return true;
     }
 }
