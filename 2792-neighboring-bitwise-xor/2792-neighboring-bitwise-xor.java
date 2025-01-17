3e57539ac14b22dbfe939ec67ff07db5c52e517c
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-01-17 09:57:26 +0530

    Time: 2 ms (100%), Space: 62.9 MB (18.24%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor=0;
+        for(int val:derived)
+        {
+            xor^=val;
+        }
+        return xor==0;
+    }
+}
