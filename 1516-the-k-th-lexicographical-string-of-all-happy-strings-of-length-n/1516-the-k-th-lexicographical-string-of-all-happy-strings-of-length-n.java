5a5e03f3f5bb71faef6b2bd3cb7093279bf1e311
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-02-19 10:40:33 +0530

    Time: 12 ms (57.63%), Space: 44.8 MB (51.25%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,21 +1,24 @@
 class Solution {
-    public void helper(String curr, int n, List<String> ans) {
+    public void helper(StringBuilder curr, int n, List<String> ans) {
         if (curr.length() == n) {
-            ans.add(curr);
+            ans.add(curr.toString());
             return;
         }
         for(char c:new char[]{'a','b','c'})
         {
             if(curr.length()==0||curr.charAt(curr.length()-1)!=c)
-            {
-                helper(curr+c,n,ans);
+            {   
+                curr.append(c);
+                helper(curr,n,ans);
+                curr.deleteCharAt(curr.length()-1);
             }
         }
     }
 
     public String getHappyString(int n, int k) {
         List<String> ans = new ArrayList<>();
-        helper("", n, ans);
+        StringBuilder sb=new StringBuilder();
+        helper(sb, n, ans);
         return ans.size() < k ? "" : ans.get(k - 1);
     }
 }
