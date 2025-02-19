3c4e6e36a0f1952d6555b11b07e50b895ea82ead
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-02-19 17:01:07 +0530

    Time: 0 ms (100%), Space: 41.6 MB (58.18%) - LeetHub

--- 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.java
+++ 1560-number-of-students-doing-homework-at-a-given-time/1560-number-of-students-doing-homework-at-a-given-time.java
@@ -1,27 +1,10 @@
-class Pair{
-    int start;
-    int end;
-    Pair(int start,int end)
-    {
-        this.start=start;
-        this.end=end;
-    }
-}
-
 class Solution {
     public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
         int n=startTime.length;
-        Pair[] pairs=new Pair[n];
+        int count=0;
         for(int i=0;i<n;i++)
         {
-            pairs[i]=new Pair(startTime[i],endTime[i]);
-        }
-        Arrays.sort(pairs,(a,b)->Integer.compare(a.start,b.start));
-        int count=0;
-        for(Pair p:pairs)
-        {
-            if(p.start<=queryTime && p.end>=queryTime) count++;
-            else if(p.start>queryTime) break;
+            if(startTime[i]<=queryTime && endTime[i]>=queryTime) count++;
         }
         return count;
     }
