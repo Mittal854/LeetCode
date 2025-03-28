d55c024dd456700a86628022d83b34ee342419dc
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-03-28 22:46:37 +0530

    Time: 2 ms (86.75%), Space: 44.8 MB (75.08%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -6,36 +6,30 @@ class Solution {
         {
             Arrays.fill(row,'.');
         }
-        solve(0,n,board,ans);
+        helper(0,n,board,ans);
         return ans;
     }
-    public void solve(int col,int n,char[][] board,List<List<String>>ans)
+    public void helper(int col,int n, char[][] board,List<List<String>>ans)
     {
         if(col==n)
         {
-            List<String> one=new ArrayList<>();
-            for(int i=0;i<n;i++)
+            List<String>board1=new ArrayList<>();
+            for(char[]row:board)
             {
-                String row="";
-                for(int j=0;j<n;j++)
-                {
-                    row=row+board[i][j];
-                }
-                one.add(new String(row));
+                String rows=new String(row);
+                board1.add(rows);
             }
-            ans.add(new ArrayList<>(one));
-            return;
+            ans.add(board1);
         }
         for(int i=0;i<n;i++)
         {
             if(isValid(i,col,board))
             {
                 board[i][col]='Q';
-                solve(col+1,n,board,ans);
+                helper(col+1,n,board,ans);
                 board[i][col]='.';
             }
         }
-
     }
     public boolean isValid(int row,int col,char[][] board)
     {
@@ -44,15 +38,14 @@ class Solution {
         {
             if(board[row][i]=='Q') return false;
         }
-        for(int i=row,j=col;i>=0 && j>=0;i--,j--)
+        for(int i=row,j=col;i>=0&&j>=0;i--,j--)
         {
             if(board[i][j]=='Q') return false;
         }
-        for(int i=row,j=col;i<n && j>=0;i++,j--)
+        for(int i=row,j=col;i<n&&j>=0;i++,j--)
         {
             if(board[i][j]=='Q') return false;
         }
-
         return true;
     }
 }
