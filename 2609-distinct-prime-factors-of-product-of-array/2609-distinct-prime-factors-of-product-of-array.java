8e6acf683ac0ee8d25e99adcfcfd7c913d152335
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-04-23 16:45:54 +0530

    Time: 9 ms (93.99%), Space: 45 MB (60.15%) - LeetHub

--- 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.java
+++ 2609-distinct-prime-factors-of-product-of-array/2609-distinct-prime-factors-of-product-of-array.java
@@ -1,23 +1,19 @@
 class Solution {
-    HashSet<Integer>factors=new HashSet<>();
-    public void prime(int num,int f)
-    {
-        if(num==1) return;
-        if(num%f==0)
-        {
-            factors.add(f);
-            prime(num/f,f);
-        }
-        else
-        {
-            prime(num,f+1);
-        }
-    }
     public int distinctPrimeFactors(int[] nums) {
-        for(int num:nums)
-        {
-            prime(num,2);
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    set.add(i);
+                    while (num % i == 0) {
+                        num /= i;
+                    }
+                }
+            }
+            if (num > 1)
+                set.add(num);
         }
-        return factors.size();
+        return set.size();
+
     }
 }
