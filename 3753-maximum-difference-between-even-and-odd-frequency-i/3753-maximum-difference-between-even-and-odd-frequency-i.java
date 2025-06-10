0810f08ab697589ac1f33e777fdc639ba5de3b40
Mittal854 <122336270+Mittal854@users.noreply.github.com>
2025-06-10 17:16:04 +0530

    Time: 1 ms (100%), Space: 42.3 MB (89.02%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.java
@@ -5,25 +5,20 @@ class Solution {
         {
             freq[c-'a']++;
         }
-        int minOdd=Integer.MAX_VALUE;
         int minEven=Integer.MAX_VALUE;
         int maxOdd=Integer.MIN_VALUE;
-        int maxEven=Integer.MIN_VALUE;
         for(int i:freq)
         {
             if(i==0) continue;
             if(i%2==0)
             {
                 minEven=Math.min(minEven,i);
-                maxEven=Math.max(maxEven,i);
             }
             else
             {
-                minOdd=Math.min(minOdd,i);
                 maxOdd=Math.max(maxOdd,i);
             }
         }
-        // return Math.max(maxOdd-minEven);
         return maxOdd-minEven;
     }
 }
